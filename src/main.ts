fb7974dfbb7dcb42943c105bc5952faa60026395
Nhlakanipho Nkokhelo <N.N.TEMBE@outlook.com>
2022-03-31 10:27:35 +0200

    update cmd

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,17 @@
-// write the cli logic
+#!/usr/bin/env node
+
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 import { League } from "./league";
-import fs from "fs";
+import { readFileSync } from "fs";
 
 const args = yargs(hideBin(process.argv)).argv as any;
 
 if (args.text) {
   try {
     const league = new League();
-    // league.displayLeaguePoints(args.text);
     console.log(league.displayLeaguePoints(args.text));
-    // console.log(args.text);
-
   } catch (e) {
-    // console.error('Please check if your text');
     console.error(e);
   }
 }
@@ -22,16 +19,13 @@ if (args.text) {
 if (args.file) {
   try {
     const league = new League();
-    const inputText = fs.readFileSync(args.file, "utf-8");
-    // league.displayLeaguePoints(inputText)
+    const inputText = readFileSync(args.file, "utf-8");
     console.log(league.displayLeaguePoints(inputText));
-    // console.log(inputText);
-
   } catch (e) {
-    // console.error('Please check if your text');
     console.error(e);
   }
 }
 
-
-// '"Lions 3, Snakes 3\nTarantulas 1, FC Awesome 0\nLions 1, FC Awesome 1\nTarantulas 3, Snakes 1\nLions 4, Grouches 0"'
+if (args.testApp) {
+  console.log("The command app is installed");
+}
